3d15f58bb52e767048f9eb50c2032694c6e2b791
LazarM <mladenmakilazarevic@gmail.com>
2020-11-01 14:00:29 -0500

    mentor module

---
+++ Spring-00-TightlyCoupled/src/FullTimeMentor.java
@@ -0,0 +1,6 @@
+public class FullTimeMentor {
+
+    public void createAccount(){
+        System.out.println("Full time mentor is Created!");
+    }
+}
---
+++ Spring-00-TightlyCoupled/src/SchoolApp.java
@@ -0,0 +1,9 @@
+public class SchoolApp {
+    public static void main(String[] args) {
+        FullTimeMentor fullTimeMentor= new FullTimeMentor();
+        PartTimeMentor partTimeMentor= new PartTimeMentor();
+
+        Mentor mentor = new Mentor(fullTimeMentor, partTimeMentor);
+        mentor.menageAccounts();
+    }
+}
