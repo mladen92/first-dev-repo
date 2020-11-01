a5be00c1aa7e035c64d33e83f9bb6317085065ae
LazarM <mladenmakilazarevic@gmail.com>
2020-11-01 17:22:37 -0500

    adding Spring Conteiner module

---
+++ Spring-00-LooselyCoupled/src/SchoolApp.java
@@ -0,0 +1,13 @@
+import implementation.Mentor;
+import service.FullTimeMentor;
+import service.MentorAccount;
+import service.PartTimeMentor;
+
+public class SchoolApp {
+    public static void main(String[] args) {
+        // FullTimeMentor fullTimeMentor = new FullTimeMentor();
+        PartTimeMentor partTimeMentor = new PartTimeMentor();
+        MentorAccount mentor = new MentorAccount(partTimeMentor);
+        mentor.manageAccount();
+    }
+}
