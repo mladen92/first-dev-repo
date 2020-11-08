42973cf7bed31a8227c0200dddf9a71382b52773
LazarM <mladenmakilazarevic@gmail.com>
2020-11-08 17:48:12 -0500

    BeanFactory container and app context

--- Spring-01-SpringContainer/src/main/java/SchoolApp.java
+++ Spring-01-SpringContainer/src/main/java/SchoolApp.java
@@ -1,5 +1,4 @@
 import interfaces.Mentor;
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
