4daada354cd2167a6cfc4e2431161d8433c88766
LazarM <mladenmakilazarevic@gmail.com>
2020-11-14 14:26:00 -0500

    MVC Controller with thymeleaf

--- Spring-08-SpringBoot-MVC-View(Thymeleaf)/src/main/java/com/app/app/controller/StudentController.java
+++ Spring-08-SpringBoot-MVC-View(Thymeleaf)/src/main/java/com/app/app/controller/StudentController.java
@@ -1,10 +1,13 @@
 package com.app.app.controller;
 
 import com.app.app.model.Student;
+import jdk.nashorn.internal.objects.annotations.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/student")
@@ -18,13 +21,21 @@ public class StudentController {
         model.addAttribute("name", name);
         model.addAttribute("course", course);
         model.addAttribute("ids", studentId);
-
-        Student student = new Student(1, "Mike", "Smith");
+        ArrayList<Integer> numArr = new ArrayList<>();
+        numArr.add(1);
+        numArr.add(2);
+        numArr.add(7);
+        numArr.add(9);
+        numArr.add(14);
+        model.addAttribute("numbers", numArr);
+        Student student = new Student(studentId, "Mike", "Smith");
         model.addAttribute("student", student);
-
-
-
-
         return "student/welcome";
+    }
+
+    @GetMapping("/register")
+    public String homePage2(){
+
+        return "student/register";
     }
 }
